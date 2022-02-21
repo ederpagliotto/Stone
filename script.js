81db30687e9505cd6789203810801e3bdd9900d8
Eder Pagliotto <ederpagliotto@gmail.com>
2022-02-21 20:22:42 +0000

    function roundGame added

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-let computer = ['Rock', 'Paper', 'Scissor'];
+let computer = ['rock', 'paper', 'scissor'];
 
 function computerPlay(computer) {                           
 return computer[Math.floor(Math.random() * computer.length)];
 }
 
-console.log(computerPlay(computer));
+let playerSelection = prompt("Your turn: ").toLowerCase();
+let computerSelection = computerPlay(computer);
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+function roundGame(playerSelection, computerSelection) {
+   if (computerSelection === playerSelection) {
+      return console.log("Empate!");
+   }
+   else if (computerSelection === "rock" && playerSelection === "paper") {
+      return console.log("You won! Paper beats rock!");
+   }
+   else if (computerSelection === "rock" && playerSelection === "scissor") {
+       return console.log("You lose! Rock beats scissor!");
+   }
+   else if (computerSelection === "paper" && playerSelection === "rock") {
+      return console.log("You lose! Paper beats rock!");
+   }
+   else if (computerSelection === "paper" && playerSelection === "scissor") {
+      return console.log("You won! Scissor beats paper!");
+   }
+   else if (computerSelection === "scissor" && playerSelection === "rock") {
+      return console.log("You won! Rock beats scissor!");
+   }
+   else {
+      return console.log("You lose! Scissor beats paper!")
+   }
+}
+
+
+console.log(roundGame(playerSelection, computerSelection));
