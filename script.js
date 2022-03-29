bceaa1c8e514f4d33f2bfbbb085b7eb567f6b1be
Eder Pagliotto <ederpagliotto@gmail.com>
2022-03-29 22:13:42 +0100

    js code finished

--- script.js
+++ script.js
@@ -2,20 +2,43 @@ let playerScore = 0;
 let computerScore = 0;
 const computerDisplay = document.getElementById('computer-display');
 const playerDisplay = document.getElementById('player-display');
+let resultDisplay = document.getElementById('result-display');
 let computer = ['rock', 'paper', 'scissor'];
+const computerScoreDisplay = document.getElementById('computer-score');
+const playerScoreDisplay = document.getElementById('player-score');
+
 
 function computerChoice(computer) {                           
 return computer[Math.floor(Math.random() * computer.length)];
 }
+
 let computerSelection;
 let playerSelection;
 const buttons = document.querySelectorAll('button');
+
+function disableButtons() {
+     buttons.forEach(e => {
+          e.disabled = true;
+     })
+}
+
 buttons.forEach((e) => e.addEventListener('click', () => {
      computerSelection = computerChoice(computer);
      playerSelection = e.value;
      playRound(playerSelection, computerSelection);
      computerDisplay.innerHTML = computerSelection;
      playerDisplay.innerHTML = playerSelection;
+     computerScoreDisplay.innerHTML = computerScore;
+     playerScoreDisplay.innerHTML = playerScore;
+
+     if (playerScore >= 5) {
+          resultDisplay.innerHTML = "Player won!"
+          disableButtons();
+       }
+     if (computerScore >= 5) {
+            resultDisplay.innerHTML = "Computer won!"
+            disableButtons();
+       }
 }))
      
 
@@ -52,6 +75,4 @@ function playRound() {
           computerScore++;
         return console.log("You lose! Scissor beats paper!");
      }
-
   }
-
